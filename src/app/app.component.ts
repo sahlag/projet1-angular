39f6dfecffda895f0daa949ef76a44ee0cce1acf
AtomicFist <36987965+AtomicFist@users.noreply.github.com>
2018-05-22 13:39:05 +0200

    integrtion materialize-liaison de donnes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Bonjour tout le monde';
+  disciption = 'Un petit exercice l\'initiaton sur Angular';
+  content: string;
+  url: string;
+  isEditable: boolean;
+
+  constructor() {
+    this.content = 'cest un texte';
+   this.url = 'https://www.facebook.com/Lhomme.expert?ref=bookmarks';
+    this.isEditable = true;
 }
+public changeStyle() {
+this.isEditable = !this.isEditable;
+}
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductlistComponent } from './productlist/productlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductlistComponent
   ],
   imports: [
     BrowserModule
