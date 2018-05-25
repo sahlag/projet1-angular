54843322871f1cd77e82a4f95d8eca97409785e8
AtomicFist <36987965+AtomicFist@users.noreply.github.com>
2018-05-25 16:56:22 +0200

    API modéfication de methode de récuperation et traitement des erreurs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
-
+// Module angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+// Modules perso
+
 import { AppRoutingModule } from './/app-routing.module';
+import { ProductModule } from './products/product.module';
+
+// Mes component
 import { HomeComponent } from './home/home.component';
 import { Error404Component } from './errors/error404/error404.component';
-import { ProductModule } from './products/product.module';
+import { AppComponent } from './app.component';
+
 // Modules pour API memoire
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMeroryDataService } from './API/in-merory-data.service';
+
 
 @NgModule({
   declarations: [
@@ -18,6 +26,7 @@ import { InMeroryDataService } from './API/in-merory-data.service';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMeroryDataService),
     ProductModule,
     AppRoutingModule
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,20 +1,35 @@
 import { Injectable } from '@angular/core';
 import { TAB_PRODUCT, Product } from '../model/product';
 import { Observable, of } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { delay, tap, catchError } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private URL = 'api/products';
+constructor(private http: HttpClient) {
+
+}
+
+
 /**
  * Retourne la liste des produits depuits un tableau en mémoire
  */
   public getProducts(): Observable <Product[]> {
     console.log('hello getProducts');
-    return of(TAB_PRODUCT).pipe(
-     delay(1000)
+
+    return this.http.get<Product[]>(this.URL).pipe(
+      tap((products) => console.log ('API a répondu: liste des produits')),
+      catchError(this.handleError('getProducts', []))
     );
   }
-
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+    console.error(error); // log to console instead
+    console.error(`${operation} echoué: ${error.message}`);
+    return of(result as T);
+    };
+    }
 }
